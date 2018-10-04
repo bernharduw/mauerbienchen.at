c0c97f07965991aaf535202f5f28d1f8aa9e0cb2
Bernhard Gschwantner <bernhard@unserwein.at>
2018-10-04 17:44:44 +0200

    changed the config

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,11 +7,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'mauerbienchen.at',
+        short_name: 'mauerbienchen',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#ffdd00',
+        theme_color: '#ffdd00',
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
