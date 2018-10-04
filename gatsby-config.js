ea008e83de01551e94b25094b787fd4387104a8e
Bernhard Gschwantner <bernhard@unserwein.at>
2018-10-04 17:56:47 +0200

    made the theme color a bit warmer

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,8 +10,8 @@ module.exports = {
         name: 'mauerbienchen.at',
         short_name: 'mauerbienchen',
         start_url: '/',
-        background_color: '#e6ac00',
-        theme_color: '#e6ac00',
+        background_color: '#fad900',
+        theme_color: '#fad900',
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
